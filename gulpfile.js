19c382beaa8bbedf4778a49d5167b9a23451beb1
Dzhamilya Ukarkhanova <d.t.ukarkhanova@yandex.ru>
2025-03-12 12:16:54 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,84 @@
+const { src, dest, task, series, parallel, watch } = require('gulp')  
+const pug = require('gulp-pug');
+const sass = require('gulp-sass')(require('sass'))  
+const browserSync = require('browser-sync').create();  
+const webpack = require('webpack');  
+const webpackConfig = require('./webpack.config.js');  
+const webpackStream = require('webpack-stream');  
+
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+
+const copy = require('gulp-copy');
+
+const clean = require('gulp-clean')
+
+// Задача очистки
+task('clean', () => {
+    return src('./build', { 
+      read: false, //ускоряет процесс, так как Gulp не анализирует содержимое файлов
+      allowEmpty: true //предотвращает ошибки при первом запуске, когда папка build еще не существует
+    })
+      .pipe(clean())
+})
+
+//Задача copy
+task('copy', () => {
+    return src('./src/public/**/*')
+      .pipe(copy('./build', { prefix: 1 })) // Копируем с сохранением структуры папок
+      .pipe(browserSync.stream())
+})
+
+// SCSS задача
+task('scss', () => {
+    return src('./src/style/**/*.scss')
+      .pipe(sourcemaps.init()) // Инициализация sourcemaps
+      .pipe(sass({ 
+        outputStyle: 'expanded' //Отключить минификацию CSS в режиме разработки
+      }).on('error', sass.logError))
+      .pipe(postcss([autoprefixer()])) // Автопрефиксы 
+      .pipe(sourcemaps.write('.')) // Запись sourcemaps 
+      .pipe(dest('./build/css'))
+      .pipe(browserSync.stream())
+  })
+
+task('pug', () => {  
+    return src('./src/pug/views/**/*.pug')  
+        .pipe(  
+            pug({  
+                // Your options in here.  
+            })  
+        )  
+        .pipe(dest('./build'))  
+        .pipe(browserSync.stream());  
+})  
+  
+task('server', () => {  
+    browserSync.init({  
+        server: { baseDir: 'build/' },  
+        notify: false,  
+        online: true  
+    })  
+})  
+  
+task('webpack', () => {  
+    return src('./src/js/main.js')  
+        .pipe(webpackStream(webpackConfig, webpack))  
+        .pipe(dest('./build/js'))  
+        .pipe(browserSync.stream());  
+})  
+  
+task('watch', () => {  
+    watch('./src/pug/**/*.pug', series('pug'));  
+    watch('./src/js/**/*.js', series('webpack')); //Watcher для всех js-файлов проекта 
+    watch('./src/style/**/*.scss', series('scss'));  // Добавляем отслеживание SCSS
+    watch('./src/public/**/*', series('copy'));  // Добавляем отслеживание public
+})  
+  
+// Обновленная задача serve
+task('serve', series(
+    'clean', // Первым шагом очищаем папку
+    parallel('pug', 'webpack', 'scss', 'copy'),  // Добавляем scss в стартовую сборку, добавляем copy
+    parallel('watch', 'server')
+));
